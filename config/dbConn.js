4ca55fcb5dfe34bc4438f62f906dc7a8a128a82b
James Pow <jbpow18@icloud.com>
2023-04-22 14:14:16 -0400

    feat: finished connectDB() function

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.export = connectDB;
